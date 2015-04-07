daed0dc2c1b0eb9ac4a12cc0f18884ed29304909
John Haley <johnh@axosoft.com>
2015-04-07 09:26:47 -0700

    Add tests for gitignores with no negative entries

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@ var LIB = require('../lib'),
     fs = require('fs');
 
 var FIXTURE = fs.readFileSync(__dirname + '/../.gitignore', 'utf8');
+var NO_NEGATIVES_FIXTURE = fs.readFileSync(__dirname + '/./.gitignore-no-negatives', 'utf8');
 
 describe('gitignore parser', function () {
   describe('parse()', function () {
@@ -10,27 +11,37 @@ describe('gitignore parser', function () {
       parsed.length.should.equal(2);
     });
   });
+
   describe('compile()', function () {
     beforeEach(function () {
       this.gitignore = LIB.compile(FIXTURE);
-    })
+      this.gitignoreNoNegatives = LIB.compile(NO_NEGATIVES_FIXTURE);
+    });
+
     describe('accepts()', function () {
       it('should accept the given filenames', function () {
         this.gitignore.accepts('test/index.js').should.be.true;
         this.gitignore.accepts('wat/test/index.js').should.be.true;
+        this.gitignoreNoNegatives.accepts('test/index.js').should.be.true;
       });
+
       it('should not accept the given filenames', function () {
         this.gitignore.accepts('test.swp').should.be.false;
         this.gitignore.accepts('node_modules/wat.js').should.be.false;
         this.gitignore.accepts('foo/bar.wat').should.be.false;
+        this.gitignoreNoNegatives.accepts('node_modules/wat.js').should.be.false;
       });
+
       it('should not accept the given directory', function () {
         this.gitignore.accepts('nonexistent').should.be.false;
         this.gitignore.accepts('nonexistent/bar').should.be.false;
+        this.gitignoreNoNegatives.accepts('node_modules').should.be.false;
       });
+
       it('should accept unignored files in ignored directories', function () {
         this.gitignore.accepts('nonexistent/foo').should.be.true;
       });
+
       it('should accept nested unignored files in ignored directories', function () {
         this.gitignore.accepts('nonexistent/foo/wat').should.be.true;
       });
@@ -41,18 +52,27 @@ describe('gitignore parser', function () {
         this.gitignore.denies('test.swp').should.be.true;
         this.gitignore.denies('node_modules/wat.js').should.be.true;
         this.gitignore.denies('foo/bar.wat').should.be.true;
+        this.gitignoreNoNegatives.denies('node_modules/wat.js').should.be.true;
       });
+
       it('should not deny the given filenames', function () {
         this.gitignore.denies('test/index.js').should.be.false;
         this.gitignore.denies('wat/test/index.js').should.be.false;
+        this.gitignoreNoNegatives.denies('test/index.js').should.be.false;
+        this.gitignoreNoNegatives.denies('wat/test/index.js').should.be.false;
       });
+
       it('should deny the given directory', function () {
         this.gitignore.denies('nonexistent').should.be.true;
         this.gitignore.denies('nonexistent/bar').should.be.true;
+        this.gitignoreNoNegatives.denies('node_modules').should.be.true;
+        this.gitignoreNoNegatives.denies('node_modules/foo').should.be.true;
       });
+
       it('should not deny unignored files in ignored directories', function () {
         this.gitignore.denies('nonexistent/foo').should.be.false;
       });
+
       it('should not deny nested unignored files in ignored directories', function () {
         this.gitignore.denies('nonexistent/foo/wat').should.be.false;
       });
@@ -62,19 +82,26 @@ describe('gitignore parser', function () {
       it('should return true for directories not mentioned by .gitignore', function () {
         this.gitignore.maybe('lib').should.be.true;
         this.gitignore.maybe('lib/foo/bar').should.be.true;
+        this.gitignoreNoNegatives.maybe('lib').should.be.true;
+        this.gitignoreNoNegatives.maybe('lib/foo/bar').should.be.true;
       });
+
       it('should return false for directories explicitly mentioned by .gitignore', function () {
         this.gitignore.maybe('baz').should.be.false;
         this.gitignore.maybe('baz/wat/foo').should.be.false;
+        this.gitignoreNoNegatives.maybe('node_modules').should.be.false;
       });
+
       it('should return true for ignored directories that have exceptions', function () {
         this.gitignore.maybe('nonexistent').should.be.true;
         this.gitignore.maybe('nonexistent/foo').should.be.true;
         this.gitignore.maybe('nonexistent/foo/bar').should.be.true;
       });
+
       it('should return false for non exceptions of ignored subdirectories', function () {
         this.gitignore.maybe('nonexistent/wat').should.be.false;
         this.gitignore.maybe('nonexistent/wat/foo').should.be.false;
+        this.gitignoreNoNegatives.maybe('node_modules/wat/foo').should.be.false;
       });
     });
   });
